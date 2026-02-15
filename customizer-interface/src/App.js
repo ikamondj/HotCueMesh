8eb6104f92896440e8355931d18d232f22146c32
tland357 <42850249+tland357@users.noreply.github.com>
2026-02-14 21:56:35 -0500

    removed default react logo

--- customizer-interface/src/App.js
+++ customizer-interface/src/App.js
@@ -147,7 +147,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <div className="panel-row">
           <Panel
             title="Triggers"
